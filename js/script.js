1ce392632ba453bf90b09c7b303a65a7f435dfd1
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-07-31 22:01:29 +0300

    hw_1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+let a = prompt('number');
+let b = prompt('number');
+
+a = Number(a);
+b = Number(b);
+
+let additionResult = a + b;
+console.log(`${a} + ${b} = ${additionResult}`);
+
+let subtractionResult = a - b;
+console.log(`${a} - ${b} = ${subtractionResult}`);
+
+let multiplicationResult = a * b;
+console.log(`${a} * ${b} = ${multiplicationResult}`);
+
+let divisionResult = a / b;
+console.log(`${a} / ${b} = ${divisionResult}`);
