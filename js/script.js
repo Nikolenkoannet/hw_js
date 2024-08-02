cd25dbcd08c648d5adc195d0c3047a5bc685b1fd
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-08-02 22:23:47 +0300

    hw_3 update

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ let b = prompt('number');
 a = Number(a);
 b = Number(b);
 
-let additionResult = a + b;
+const additionResult = a + b;
 console.log(`${a} + ${b} = ${additionResult}`);
 
-let subtractionResult = a - b;
+const subtractionResult = a - b;
 console.log(`${a} - ${b} = ${subtractionResult}`);
 
-let multiplicationResult = a * b;
+const multiplicationResult = a * b;
 console.log(`${a} * ${b} = ${multiplicationResult}`);
 
-let divisionResult = a / b;
+const divisionResult = a / b;
 console.log(`${a} / ${b} = ${divisionResult}`);
