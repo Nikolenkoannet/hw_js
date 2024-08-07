bc89e6d0e568d7b4047e7d018602e0fa73841485
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-08-07 22:17:40 +0300

    hw_8 initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 'use strict'
 
+let number1 = prompt("Введіть число 1");
+let number2 = prompt("Введіть число 2");
+let number3 = prompt("Введіть число 3");
 
+number1 = Number(number1);
+number2 = Number(number2);
+number3 = Number(number3);
 
+const average = (number1 + number2 + number3) / 3;
+
+alert(`${average}`);
