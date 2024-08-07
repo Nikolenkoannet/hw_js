ca9fb1690c8884212fe256c35e4ba60492e7a2bc
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-08-07 20:06:06 +0300

    hw_6 update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict'
 
 
-let name = prompt("введіть ваше імя");
-let age = prompt("введіть ваш вік");
-let likesProgramming = confirm("Чи подобається вам програмувати?");
+const name = prompt("введіть ваше імя");
+const age = prompt("введіть ваш вік");
+const likesProgramming = confirm("Чи подобається вам програмувати?");
 
 console.log("Ім'я:", name);
 console.log("Вік:", age);
@@ -16,24 +16,24 @@ console.log("Чи подобається програмувати:",typeof likes
 
 
 
-let name = prompt("Введіть своє ім'я:");
-let greeting = `Привіт, ${name}!`;
+const name = prompt("Введіть своє ім'я:");
+const greeting = `Привіт, ${name}!`;
 
 alert(greeting);
 
 
 
-let name = prompt("Введіть ім'я:");
-let age = prompt("Введіть вік:");
-let numericAge = Number(age) ;
-let nextAge = numericAge + 1;
+const name = prompt("Введіть ім'я:");
+const age = prompt("Введіть вік:");
+const numericAge = Number(age) ;
+const nextAge = numericAge + 1;
 console.log(nextAge);
 
 
-let name = prompt("Введіть ім'я:");
-let age = prompt("Введіть вік:");
-let numericAge = Number(age);
-let nextAge = numericAge + 1;
+const name = prompt("Введіть ім'я:");
+const age = prompt("Введіть вік:");
+const numericAge = Number(age);
+const nextAge = numericAge + 1;
 console.log(nextAge);
-let isAdult = numericAge >= 18;
+const isAdult = numericAge >= 18;
 console.log(isAdult);
