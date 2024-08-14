8e9256ac69c51fd7a12a77ee36ad121a651e3fc2
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-08-14 23:51:05 +0300

    hw_10 initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 'use strict'
 
+const yearOfBirth = prompt('Введіть ваш рік народження');
+const currentYear = new Date().getFullYear();
+const  currentAge = currentYear - yearOfBirth;
+console.log(currentAge);
 
 
+const countryCityMap = {
+    kyiv: 'Ukraine',
+    washington: 'USA',
+    london: 'Great Britain',
+};
+const city = prompt('Введіть ваше місто');
+countryCityMap[city.toLowerCase()] ? console.log(countryCityMap[city.toLowerCase()]) : null;
+
+
+const sportChampionMap = {
+    football: 'Lionel Messi',
+    basketball: 'Michael Jordan',
+    boxing: 'Oleksandr Usyk'
+};
+const sport = prompt('Введіть ваш вид спорту');
+sportChampionMap[sport.toLowerCase()] ? console.log(sportChampionMap[sport.toLowerCase()]) : null;
