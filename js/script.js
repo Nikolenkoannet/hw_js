96ff44225b78fe2abf8a2c3a491cec6aa2a9099e
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-08-26 23:58:33 +0300

    hw_16 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 'use strict'
 
+function padString(str, length, padding = ' ', left = false) {
+
+    if (typeof str !== 'string') return 'Error: Invalid string';
+    if (typeof length !== 'number') return 'Error: Invalid length';
+    if (typeof padding !== 'string' || padding.length !== 1) return 'Error: Invalid padding character';
+
+    if (str.length > length) {
+        return str.substring(0, length);
+    }
+
+    let paddingString = padding.repeat(length - str.length);
+
+    if (left) {
+        return paddingString + str;
+    } else {
+        return str + paddingString;
+    }
+}
+
+console.log(padString('hello', 8, '*'));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 6, '*', true));
+console.log(padString('hello', 2));
 
