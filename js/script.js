32fd0121de2c4966a7e9efceabbfc6661debf8d0
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-09-02 23:54:04 +0300

    hw_18 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 'use strict'
 
+function averageOfNumbers(arr) {
+    let sum = 0;
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number') {
+            sum += arr[i];
+            count++;
+        }
+    }
+
+    return sum / count;
+}
+const array = [10,'Anna',4, 6,'Hello', 20, 15];
+const result = averageOfNumbers(array);
+console.log(result);
+
+
+function doMath(x, znak, y) {
+    switch (znak) {
+        case '+': return x + y;
+        case '-': return x - y;
+        case '*': return x * y;
+        case '/': return x / y;
+        case '%': return x % y;
+        case '^': return Math.pow(x, y);
+    }
+}
+const x = +prompt("Введіть перше число:");
+const znak = prompt("Оберіть (+, -, *, /, %, ^):");
+const y = +prompt("Введіть друге число:");
+alert(doMath(x, znak, y));
+
 
