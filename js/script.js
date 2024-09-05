71520a825763cfd5a6e0be77434065c7a1a6509d
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-09-05 22:48:47 +0300

    hw_19 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 'use strict'
 
 
+
+function mainFunction(callback) {
+
+    let numberInput = prompt("Введіть число:");
+    let exponentInput = prompt("Введіть ступінь:");
+
+    let number = Number(numberInput);
+    let exponent = Number(exponentInput);
+
+    callback(number, exponent);
+}
+function exponentiation(number, exponent) {
+
+    let result = number ** exponent;
+    alert(`Результат: ${result}`);
+}
+mainFunction(exponentiation);
+
+function multiplay(number, exponent) {
+    let result = number * exponent;
+    alert(`Результат: ${result}`);
+}
+mainFunction(multiplay);
+
+function division(number, exponent) {
+    let result = number / exponent;
+    alert(`Результат: ${result}`);
+}
+mainFunction(division);
+
+function modulo(number, exponent) {
+    let result = number % exponent;
+    alert(`Залишок від ділення: ${result}`);
+}
+mainFunction(modulo);
