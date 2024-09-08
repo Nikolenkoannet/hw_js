80863172775fe6e922db2a1809ed9e42f97fb092
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-09-08 22:04:04 +0300

    hw_21 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 'use strict'
 
 
+const inputString = prompt('Введіть довжину');
+const length = Number(inputString);
+
+const array = [];
+
+for (let i = 0; i < length; i++) {
+    const element = prompt(`Введіть елемент ${i+1}:`);
+    array.push(element);
+}
+
+console.log(array);
+
+array.sort();
+console.log(array)
+
+array.splice(2, 3,);
+console.log(array)
+
+
+
