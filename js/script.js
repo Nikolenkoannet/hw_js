f22b28ecb50f51fe3d7984ec794d52ce0b2c90a2
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-09-20 21:54:18 +0300

    hw_28 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 'use strict'
 
+const recursiveFactorial = (n) => {
+    return (n === 0 || n === 1) ? 1 : n * recursiveFactorial(n - 1);
+};
+console.log(recursiveFactorial(3));
 
+
+
+const calculatePower = (value, power) => {
+    if (power === 0) {
+        return 1;
+    }
+    return value * calculatePower(value, power - 1);
+};
+console.log(calculatePower(2, 2));
+
+
+
+const sumRecursive  = (a, b) => {
+
+    if (b === 0) {
+        return a;
+    }
+    return sumRecursive (a + 1, b - 1);
+};
+console.log(sumRecursive (2, 2));
