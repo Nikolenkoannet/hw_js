f69720b7c7caac4a43228c42536fdd364ac1c464
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-09-22 20:25:14 +0300

    hw_27 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 'use strict'
 
 
+const user = {
+    firstName: 'Anna',
+    lastName: 'Nikolenko'
+};
+
+const getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+};
+
+
+const bind = (func, context) => {
+
+    return function() {
+        context.tempFunc = func;
+        const result = context.tempFunc();
+        delete context.tempFunc;
+        return result;
+    };
+};
+
+const bindedGetFullName = bind(getFullName, user);
+console.log(bindedGetFullName());
