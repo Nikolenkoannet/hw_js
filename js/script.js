019813bf0289738709dee19291ebc28dcd12a362
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-09-25 16:05:13 +0300

    hw_30 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 'use strict'
 
 
+function SumCounter() {
+    let total = 0;
+
+    function addTotal(number) {
+        total += number;
+        return total;
+    }
+    return addTotal;
+}
+
+const add = SumCounter();
+
+console.log(add(3));  // = 3
+console.log(add(5));  // = 8
+console.log(add(20)); // = 28
