86475fac5b10296e4b01d198753bb0d7a5becc8a
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-09-25 16:16:06 +0300

    hw_31 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 'use strict'
 
 
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+        return this;
+    }
+};
+
+// Тепер можливо використати по ланцюжку:
+ladder.up().up().down().showStep();
