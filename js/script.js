699ca01e5f509b7dad213167dcd3da5a98089f36
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-10-02 23:20:24 +0300

    hw_34 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 'use strict'
 
+let input = document.getElementById('input');
+let ghost = document.getElementById('ghost');
 
+
+input.onfocus = () => {
+    ghost.style.display = 'block';
+};
+
+input.onblur = () => {
+    ghost.style.display = 'none';
+};
