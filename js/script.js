696d536f7edb0d6a3edc3e1d5e2c0d04110fc48d
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-10-03 00:02:01 +0300

    hw_32 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 'use strict'
 
+let table = document.getElementById('myTable');
+let counter = 1;
 
+for (let i = 0; i < 10; i++) {
+    let row = "<tr>";
+
+    for (let b = 0; b < 10; b++) {
+        row += "<td>" + counter + "</td>";
+        counter++;
+    }
+
+    row += "</tr>";
+    table.innerHTML += row;
+}
