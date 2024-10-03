798bf7011bb4edf0e83aad558bca7cb57c4418fe
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-10-03 23:59:24 +0300

    hw_33 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 'use strict'
 
+let randomNumber = Math.floor(Math.random() * 9) + 1;
+let imagePath = "images/" + randomNumber + ".jpg";
 
+console.log(imagePath);
+
+document.body.innerHTML = `<img src="${imagePath}" alt="Random Image" width="300">`;
