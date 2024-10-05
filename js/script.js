bdf122ef84f18deb9cc8e8512aca279e3255154f
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-10-05 18:20:23 +0300

    hw_33 update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
 'use strict'
 
-let table = document.getElementById('myTable');
-let counter = 1;
 
-for (let i = 0; i < 10; i++) {
-    let row = "<tr>";
+function generateContent(counter) {
+    return counter;
+}
 
-    for (let b = 0; b < 10; b++) {
-        row += "<td>" + counter + "</td>";
-        counter++;
+function generateTable(rows, columns) {
+    let table = "<table>";
+    let counter = 1;
+
+    for (let i = 0; i < rows; i++) {
+        let row = "<tr>";
+
+        for (let b = 0; b < columns; b++) {
+            row += "<td>" + generateContent(counter) + "</td>";
+            counter++;
+        }
+
+        row += "</tr>";
+        table += row;
     }
 
-    row += "</tr>";
-    table.innerHTML += row;
+    table += "</table>";
+    return table;
 }
+
+function insertTableToPage(table) {
+    document.body.innerHTML += table;
+}
+
+let table = generateTable(10, 10);
+insertTableToPage(table);
