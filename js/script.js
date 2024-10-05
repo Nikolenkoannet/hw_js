7f51368a51e263c27b3a15de13dd909880ce6e34
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-10-05 18:43:03 +0300

    hw_34 update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 'use strict'
 
+function createElements() {
+    document.body.innerHTML += `
+        <input type="text" id="input">
+        <div id="ghost" style="display: none">Ghost</div>
+    `;
+}
+createElements();
+
 let input = document.getElementById('input');
 let ghost = document.getElementById('ghost');
-
 
 input.onfocus = () => {
     ghost.style.display = 'block';
