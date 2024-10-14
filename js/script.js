9c270bae36ea8dd58d1e917bb7851b4222db51fa
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-10-14 22:55:25 +0300

    hw_37 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-'use strict'
+'use strict' ;
+
+(function () {
+
+    const form = document.querySelector('[data-todo-form]');
+    const todoItemsContainer = document.querySelector('#todoItems');
+
+    const createTodoItem = (title, description) => {
+        const item = document.createElement('div');
+        item.className = 'taskWrapper';
+        item.innerHTML = `<div class="taskHeading">${title}</div>
+                          <div class="taskDescription">${description}</div>`;
+        return item;
+    }
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const title = form.querySelector('input[name="title"]').value;
+        const description = form.querySelector('textarea[name="description"]').value;
 
 
+
+        if (!title || !description) {
+            alert("Please fill in all fields!");
+            return;
+        }
+
+        const todoItemElement = createTodoItem(title, description);
+        todoItemsContainer.prepend(todoItemElement);
+        form.reset();
+    }
+
+    form.addEventListener('submit', submitHandler);
+
+})();
