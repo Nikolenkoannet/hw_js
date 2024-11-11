33ece574034db4c1f378fc779cf902d6ef5ba709
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-11-11 23:22:59 +0200

    hw_40 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 'use strict'
 
+const user = {
+    _name: 'Anna',
+    _age: 27,
 
+    get name() {
+        return this._name;
+    },
+
+    set name(newName) {
+        this._name = newName;
+    },
+
+    get age() {
+        return this._age;
+    },
+
+    set age(newAge) {
+        if (newAge >= 0 && newAge <= 120) {
+            this._age = newAge;
+        } else {
+            console.error('error: age must be greater than or equal to 120');
+        }
+    }
+};
+console.log(user.name);
+console.log(user.age)
