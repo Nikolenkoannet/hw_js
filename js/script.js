e6aa12eef63c98ffec37e23e9d425b66a137d3e0
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-12-08 20:41:48 +0200

    hw_35 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 'use strict'
 
+const items = document.querySelectorAll('ul > li');
+items.forEach(item => console.log(item.textContent));
 
+const ulElement = document.querySelector('ul');
+const attributesValues = [];
+for (let attr of ulElement.attributes) {
+    attributesValues.push(attr.value);
+}
+console.log(attributesValues);
+
+const attributesNames = [];
+for (let attr of ulElement.attributes) {
+    attributesNames.push(attr.name);
+}
+console.log(attributesNames);
+
+const lastItem = ulElement.lastElementChild;
+lastItem.textContent = 'Hello, my name is Ann'
+
+const firstItem = ulElement.firstElementChild;
+firstItem.setAttribute('data-my-name', 'Ann');
+
+ulElement.removeAttribute('data-dog-tail');
