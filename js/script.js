52a8ffda47daf1e55b921add185155bbd59902f3
Nikolenkoannet <nickolencko.anna@gmail.com>
2024-12-14 23:35:01 +0200

    hw_45 initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 'use strict'
 
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+class TaskQueue {
+    constructor(delayMs) {
+        this.delayMs = delayMs;
+        this.tasks = [];
+    }
+    addTask(task) {
+        this.tasks.push(task);
+    }
+    run() {
+        let promiseChain = Promise.resolve();
+
+        this.tasks.forEach((task) => {
+            promiseChain = promiseChain
+                .then(() => task())
+                .then((result) => {
+                    console.log(result);
+                    return delay(this.delayMs);
+                });
+        });
+
+        return promiseChain.then(() => "Усі завдання завершено");
+    }
+}
+
+const queue = new TaskQueue(1000);
+
+queue.addTask(() => Promise.resolve("Task 1 виконано"));
+queue.addTask(() => Promise.resolve("Task 2 виконано"));
+queue.addTask(() => Promise.resolve("Task 3 виконано"));
+
+queue.run().then((message) => {
+    console.log(message);
+});
+
 
